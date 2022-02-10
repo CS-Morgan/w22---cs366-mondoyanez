bd5744a188e68f9993ff558bf7cf8a71c2ab070b
Armando Yanez <ayanez20@wou.edu>
2022-02-10 14:39:02 -0800

    completed C# language essentials

---
+++ C# Language Essentials/LanguageFeatures/Models/IProductionSelection.cs
@@ -0,0 +1,21 @@
+﻿//using System.Collections.Generic;
+
+//namespace LanguageFeatures.Models
+//{
+//    public interface IProductionSelection
+//    {
+//        IEnumerable<Product> Products { get; }
+//    }
+//}
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LanguageFeatures.Models
+{
+    public interface IProductionSelection
+    {
+        IEnumerable<Product> Products { get; }
+        IEnumerable<string> Names => Products.Select(p => p.Name);
+    }
+}

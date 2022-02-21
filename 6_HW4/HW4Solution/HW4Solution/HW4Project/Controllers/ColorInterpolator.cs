6eed0851376278715e5d144f6447ca748cc8f0aa
Armando Yanez <ayanez20@wou.edu>
2022-02-20 19:17:14 -0800

    Started implementation of colorInterpolation webpage

---
+++ 6_HW4/HW4Solution/HW4Solution/HW4Project/Controllers/ColorInterpolator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HW4Project.Controllers
+{
+    public class ColorInterpolator : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}

cef26b4938b6dde69ebe1136a4b7f70c5a4c9c39
Armando Yanez <ayanez20@wou.edu>
2022-02-23 22:11:46 -0800

    Began implementing color interpolation model

--- 6_HW4/HW4Solution/HW4Solution/HW4Project/Controllers/ColorInterpolator.cs
+++ 6_HW4/HW4Solution/HW4Solution/HW4Project/Controllers/ColorInterpolator.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HW4Project.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HW4Project.Controllers
 {
     public class ColorInterpolator : Controller
     {
+        //[HttpPost]
+        //public IActionResult Create(ColorInterpolation c)
+        //{
+        //    return View("Create", c);
+        //}
+
         public IActionResult Create()
         {
             return View();
--- 6_HW4/HW4Solution/HW4Solution/HW4Project/Models/Color.cs
+++ 6_HW4/HW4Solution/HW4Solution/HW4Project/Models/Color.cs
@@ -1,4 +1,6 @@
-﻿namespace HW4Project.Models
+﻿using System;
+
+namespace HW4Project.Models
 {
     public class Color
     {
@@ -9,6 +11,35 @@
         public string RGBToHex()
         {
            return red?.ToString("X2") + green?.ToString("X2") + blue?.ToString("X2");
+        }
+
+        public double GetHue()
+        {
+            int min = Math.Min((int)red, Math.Min((int)green, (int)blue));
+            double hue;
+
+            if (red > green && red > blue)
+            {
+                int max = (int)red;
+                hue = (double)(green - blue) / (max - min);
+            }
+            else if (green > red && green > blue)
+            {
+                int max = (int)green;
+                hue = (double)(2.0 + (blue - red) / (max - min));
+            }
+            else
+            {
+                int max = (int)blue;
+                hue = (double)(4.0 + (red - green) / (max - min));
+            }
+
+            return hue;
+        }
+
+        public static Color FromArgb(int v1, int v2, int t, int p)
+        {
+            throw new NotImplementedException();
         }
     }
 }

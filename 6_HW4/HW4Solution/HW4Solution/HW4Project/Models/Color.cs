66fcde59b9d1a7124320a6c3430a87cbf9a48255
Armando Yanez <ayanez20@wou.edu>
2022-02-27 14:07:01 -0800

    Implmentated RGB to HSV and HSV back to RGB

--- 6_HW4/HW4Solution/HW4Solution/HW4Project/Models/Color.cs
+++ 6_HW4/HW4Solution/HW4Solution/HW4Project/Models/Color.cs
@@ -10,36 +10,58 @@ namespace HW4Project.Models
 
         public string RGBToHex()
         {
-           return red?.ToString("X2") + green?.ToString("X2") + blue?.ToString("X2");
+            return red?.ToString("X2") + green?.ToString("X2") + blue?.ToString("X2");
+        }
+
+        public Color HexToColor(string hex)
+        {
+            string RGBR = hex.Substring(0, 2);
+            int r = Convert.ToInt32(RGBR, 16);
+
+            string RGBG = hex.Substring(2, 2);
+            int g = Convert.ToInt32(RGBR, 16);
+
+            string RGBB = hex.Substring(4, 2);
+            int b = Convert.ToInt32(RGBB, 16);
+
+            Color color = new Color() { red = r, green = g, blue = blue };
+
+            return color;
         }
 
         public double GetHue()
         {
-            int min = Math.Min((int)red, Math.Min((int)green, (int)blue));
-            double hue;
+            double hue = 0;
 
-            if (red > green && red > blue)
+            double redPrime = (double)red / 255;
+            double greenPrime = (double)green / 255;
+            double bluePrime = (double)blue / 255;
+
+            double colorMax = Math.Max(redPrime, Math.Max(greenPrime, bluePrime));
+            double colorMin = Math.Min(redPrime, Math.Min(greenPrime, bluePrime));
+
+            double difference = colorMax - colorMin;
+
+            if (difference.Equals(0))
             {
-                int max = (int)red;
-                hue = (double)(green - blue) / (max - min);
+                hue = 0;
             }
-            else if (green > red && green > blue)
+            else if (colorMax.Equals(redPrime))
             {
-                int max = (int)green;
-                hue = (double)(2.0 + (blue - red) / (max - min));
+                hue = Math.Round((greenPrime - bluePrime) / difference);
+                hue = hue % 6;
+                hue = hue * 60;
             }
-            else
+            else if (colorMax.Equals(greenPrime))
             {
-                int max = (int)blue;
-                hue = (double)(4.0 + (red - green) / (max - min));
+                hue = Math.Round(60 * (((bluePrime - redPrime) / difference) + 2));
+            }
+            else if (colorMax.Equals(bluePrime))
+            {
+                hue = Math.Round(60 * (((redPrime - greenPrime) / difference) + 4));
             }
 
             return hue;
-        }
-
-        public static Color FromArgb(int v1, int v2, int t, int p)
-        {
-            throw new NotImplementedException();
         }
     }
 }

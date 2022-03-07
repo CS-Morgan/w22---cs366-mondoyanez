8786167a1b96125123921e640b0cf324baaa169c
Armando Yanez <ayanez20@wou.edu>
2022-03-06 19:12:14 -0800

    Added text columns and buttons to GenerateMadLibs webpage

--- 7_Final_Project/MadLibsSolution/MadLibs/Controllers/MadLibController.cs
+++ 7_Final_Project/MadLibsSolution/MadLibs/Controllers/MadLibController.cs
@@ -13,15 +13,23 @@ namespace MadLibs.Controllers
     {
 
         [HttpGet]
-        public IActionResult GenerateMadLibs()
+        public IActionResult GenerateMadLibs(string n1, string aj1, string n2, string aj2, string aj3, string v1, string v2, int num1, string v3, string av1,
+        string n3, string c1, string v4, string n4, string e1, string v5, string av2, string n5, string v6, string v7, string n6, string pn, string v8, 
+        string name, string v9, string n7, string v10, string aj4, string av3, string av4, string n8, string aj5, string n9, string aj6)
         {
-            return View();
+            Madlib madlib = new Madlib() 
+            {
+                Noun1 = n1, Adjective1 = aj1, Noun2 = n2, Adjective2 = aj2, Adjective3 = aj3, Verb1 = v1, Verb2 = v2, Number1 = num1, Verb3 = v3, Adverb1 = av1, Noun3 = n3, 
+                Color1 = c1, Verb4 = v4, Noun4 = n4, Exlamation1 = e1, Verb5 = v5, Adverb2 = av2, Noun5 = n5, Verb6 = v6, Verb7 = v7, Noun6 = n6, PluralNoun = pn,  Verb8 = v8, 
+                Name = name, Verb9 = v9, Noun7 = n7, Verb10 = v10, Adjective4 = aj4, Adverb3 = av3, Adverb4 = av4, Noun8 = n8, Adjective5 = aj5, Noun9 = n9, Adjective6 = aj6
+            };
+            return View("GenerateMadLibs", madlib);
         }
 
         [HttpGet]
-        public IActionResult DisplayMadLibs()
+        public IActionResult DisplayMadLibs(Madlib madlib)
         {
-            return View();
+            return View("DisplayMadLibs", madlib);
         }
 
         // [HttpPost]
---
+++ 7_Final_Project/MadLibsSolution/MadLibs/Models/Madlib.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace MadLibs.Models
+{
+    public class Madlib
+    {
+        public string Noun1 { get; set; }
+        public string Adjective1 { get; set; }
+        public string Noun2 { get; set; }
+        public string Adjective2 { get; set; }
+        public string Adjective3 { get; set; }
+        public string Verb1 { get; set; }
+        public string Verb2 { get; set; }
+        public int Number1 { get; set; }
+        public string Verb3 { get; set; }
+        public string Adverb1 { get; set; }
+        public string Noun3 { get; set; }
+        public string Color1 { get; set; }
+        public string Verb4 { get; set; }
+        public string Noun4 { get; set; }
+        public string Exlamation1 { get; set; }
+        public string Verb5 { get; set; }
+        public string Adverb2 { get; set; }
+        public string Noun5 { get; set; }
+        public string Verb6 { get; set; }
+        public string Verb7 { get; set; }
+        public string Noun6 { get; set; }
+        public string PluralNoun { get; set; }
+        public string Verb8 { get; set; }
+        public string Name { get; set; }
+        public string Verb9 { get; set; }
+        public string Noun7 { get; set; }
+        public string Verb10 { get; set; }
+        public string Adjective4 { get; set; }
+        public string Adverb3 { get; set; }
+        public string Adverb4 { get; set; }
+        public string Noun8 { get; set; }
+        public string Adjective5 { get; set; }
+        public string Noun9 { get; set; }
+        public string Adjective6 { get; set; }
+    }
+}
